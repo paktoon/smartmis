c4ce11197da2b790916380d580c2fbbb8da3c01a
paktoon <paktoon@gmail.com>
2013-10-28 00:10:07 +0700

    Update version

--- src/com/smart/mis/client/MainPage.java
+++ src/com/smart/mis/client/MainPage.java
@@ -364,7 +364,7 @@ public class MainPage {
 		
 		ToolStrip toolStripVersion = new ToolStrip();
         toolStripVersion.setWidth100();
-        Label version = new Label("Version: 0.3 <br> Built Sat 10/20/2013 11:58 PM");
+        Label version = new Label("Version: 0.4 <br> Built Sat 10/28/2013 12:09 AM");
         version.setWidth100();
         version.setPadding(5);
         toolStripVersion.addMember(version);
