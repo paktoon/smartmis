0d0e5aec930931680511b620f9f11bf462bdab10
paktoon <paktoon@gmail.com>
2013-12-19 03:01:33 +0700

    Update 12/19/2013

--- src/com/smart/mis/client/function/sale/report/quotation/ReportQuotationLayout.java
+++ src/com/smart/mis/client/function/sale/report/quotation/ReportQuotationLayout.java
@@ -3,10 +3,13 @@ package com.smart.mis.client.function.sale.report.quotation;
 //import java.util.Calendar;
 import java.util.Date;
 
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.smartgwt.client.widgets.Label;
 import com.smart.mis.client.function.sale.quotation.QuotationDS;
+import com.smart.mis.shared.DateTimeMapping;
 import com.smart.mis.shared.EditorListGrid;
 import com.smart.mis.shared.FieldFormatter;
+import com.smart.mis.shared.purchasing.PurchaseOrderStatus;
 import com.smart.mis.shared.security.User;
 import com.smartgwt.client.data.AdvancedCriteria;
 import com.smartgwt.client.data.Criterion;
@@ -20,6 +23,7 @@ import com.smartgwt.client.types.SortDirection;
 import com.smartgwt.client.types.SummaryFunctionType;
 import com.smartgwt.client.types.TitleOrientation;
 import com.smartgwt.client.types.VerticalAlignment;
+import com.smartgwt.client.util.SC;
 import com.smartgwt.client.widgets.IButton;
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
@@ -27,8 +31,11 @@ import com.smartgwt.client.widgets.form.DynamicForm;
 import com.smartgwt.client.widgets.form.fields.DateItem;
 import com.smartgwt.client.widgets.form.fields.FormItemIcon;
 import com.smartgwt.client.widgets.form.fields.PickerIcon;
+import com.smartgwt.client.widgets.form.fields.SelectItem;
 import com.smartgwt.client.widgets.form.fields.StaticTextItem;
 import com.smartgwt.client.widgets.form.fields.TextItem;
+import com.smartgwt.client.widgets.form.fields.events.ChangedEvent;
+import com.smartgwt.client.widgets.form.fields.events.ChangedHandler;
 import com.smartgwt.client.widgets.form.fields.events.IconClickEvent;
 import com.smartgwt.client.widgets.form.fields.events.IconClickHandler;
 import com.smartgwt.client.widgets.form.fields.events.KeyPressEvent;
@@ -47,10 +54,20 @@ public class ReportQuotationLayout extends VLayout{
 	ListGrid reportListGrid;
 	Label reportDate;
 	DynamicForm searchForm;
-	DateItem searchItem;
+	//DateItem searchItem;
 	IButton viewButton;
 	
+	String select_day;
+	String select_month;
+	String select_year;
+	
 	public ReportQuotationLayout(final User currentUser) {
+		
+		select_day = DateTimeFormat.getFormat( "d-M-yyyy" ).format( new Date() ).split( "-")[0];
+		select_month = DateTimeFormat.getFormat( "d-M-yyyy" ).format( new Date() ).split( "-")[1];
+		select_year = DateTimeFormat.getFormat( "d-M-yyyy" ).format( new Date() ).split( "-")[2];
+		select_year = Integer.toString((Integer.parseInt(select_year) + 543));
+		
 		setWidth(950);
 		setHeight100();
 		
@@ -61,38 +78,98 @@ public class ReportQuotationLayout extends VLayout{
 		search.setMembersMargin(5);
 		
 		searchForm = new DynamicForm();
-		searchItem = new DateItem("created_date", "วันที่ออกรายงาน");
-        searchItem.setTitleOrientation(TitleOrientation.TOP);
-        searchItem.setColSpan(2);
-        searchItem.setTitleAlign(Alignment.LEFT);
-        searchItem.addKeyPressHandler(new KeyPressHandler() {
-            public void onKeyPress(KeyPressEvent event) {
-                if("enter".equalsIgnoreCase(event.getKeyName())) {
-                    updateDetails();
-                }
-            }
-        });
-        searchItem.setDefaultChooserDate(new Date());
-        searchItem.setDefaultValue(new Date());
-        searchItem.setUseTextField(true);
+		searchForm.setWidth(200);
+		searchForm.setNumCols(3);
+		searchForm.setTitleOrientation(TitleOrientation.TOP);
+//		searchItem = new DateItem("created_date", "วันที่ออกรายงาน");
+//        searchItem.setTitleOrientation(TitleOrientation.TOP);
+//        searchItem.setColSpan(2);
+//        searchItem.setTitleAlign(Alignment.LEFT);
+//        searchItem.addKeyPressHandler(new KeyPressHandler() {
+//            public void onKeyPress(KeyPressEvent event) {
+//                if("enter".equalsIgnoreCase(event.getKeyName())) {
+//                    updateDetails();
+//                }
+//            }
+//        });
+//        searchItem.setDefaultChooserDate(new Date());
+//        searchItem.setDefaultValue(new Date());
+//        searchItem.setUseTextField(true);
+		
+		final SelectItem dayItem = new SelectItem("dayItem", "วันที่");
+		dayItem.setValueMap(DateTimeMapping.getDateValueMap(DateTimeMapping.getNumDay(select_month, select_year)));
+		dayItem.setDefaultValue(select_day);
+		dayItem.setWidth(50);
+		final SelectItem monthItem = new SelectItem("monthItem", "เดือน");
+		monthItem.setValueMap(DateTimeMapping.getMonthValueMap());
+		monthItem.setDefaultValue(DateTimeMapping.getDisplay(select_month));
+		monthItem.setWidth(100);
+		final SelectItem yearItem = new SelectItem("yearItem", "ปี");
+		yearItem.setValueMap(DateTimeMapping.getYearPast(select_year,5), DateTimeMapping.getYearPast(select_year,4) , DateTimeMapping.getYearPast(select_year,3), DateTimeMapping.getYearPast(select_year,2), DateTimeMapping.getYearPast(select_year,1),select_year);
+		yearItem.setDefaultValue(select_year);
+		yearItem.setWidth(50);
         
         final PickerIcon findIcon = new PickerIcon(PickerIcon.SEARCH);
         final PickerIcon cancelIcon = new PickerIcon(PickerIcon.CLEAR);
-        searchItem.setIcons(findIcon, cancelIcon);
+        yearItem.setIcons(findIcon, cancelIcon);
         
-        searchItem.addIconClickHandler(new IconClickHandler() {
+        yearItem.addIconClickHandler(new IconClickHandler() {
             public void onIconClick(IconClickEvent event) {
                 FormItemIcon icon = event.getIcon();
                 if(icon.getSrc().equals(cancelIcon.getSrc())) {
                 	searchForm.reset();
+            		select_day = DateTimeFormat.getFormat( "d-M-yyyy" ).format( new Date() ).split( "-")[0];
+            		select_month = DateTimeFormat.getFormat( "d-M-yyyy" ).format( new Date() ).split( "-")[1];
+            		select_year = DateTimeFormat.getFormat( "d-M-yyyy" ).format( new Date() ).split( "-")[2];
                 	resetDetails();
-                } else {
-                	updateDetails();
+                } else if(icon.getSrc().equals(findIcon.getSrc())) {
+                	if (select_day != null) updateDetails();
+                	else SC.warn("กรุณาเลือกวันที่");
                 }
             }
         });
         
-        searchForm.setFields(searchItem);
+        dayItem.addChangedHandler(new ChangedHandler(){
+
+			@Override
+			public void onChanged(ChangedEvent event) {
+				select_day = dayItem.getValueAsString();
+			}
+        });
+        
+        monthItem.addChangedHandler(new ChangedHandler(){
+
+			@Override
+			public void onChanged(ChangedEvent event) {
+				int num_1 = DateTimeMapping.getNumDay(select_month, select_year);
+				select_month = monthItem.getValueAsString();
+				int num_2 = DateTimeMapping.getNumDay(select_month, select_year);
+				if (num_1 != num_2) {
+					select_day = "1";
+					dayItem.setValue(select_day);
+					dayItem.setValueMap(DateTimeMapping.getDateValueMap(num_2));
+				}
+			}
+        	
+        });
+        
+        yearItem.addChangedHandler(new ChangedHandler(){
+
+			@Override
+			public void onChanged(ChangedEvent event) {
+				int num_1 = DateTimeMapping.getNumDay(select_month, select_year);
+				select_year = yearItem.getValueAsString();
+				int num_2 = DateTimeMapping.getNumDay(select_month, select_year);
+				if (num_1 != num_2) {
+					select_day = "1";
+					dayItem.setValue(select_day);
+					dayItem.setValueMap(DateTimeMapping.getDateValueMap(num_2));
+				}
+			}
+        	
+        });
+        
+        searchForm.setFields(dayItem, monthItem, yearItem);
         search.addMember(searchForm);
         
 //        VLayout buttonLayout = new VLayout();
@@ -115,15 +192,18 @@ public class ReportQuotationLayout extends VLayout{
         addMember(search);
         
         VLayout header = new VLayout();
-        header.setAlign(Alignment.CENTER);
         header.setWidth(950);
         header.setHeight(10);
         Label text = new Label();
-        text.setText("รายงานการเสนอราคา");
-        text.setHeight("10");
+        text.setContents("รายงานการเสนอราคา");
+        text.setAlign(Alignment.CENTER);
+        text.setHeight(10);
+        text.setStyleName("printTitle");
         reportDate = new Label();
-        reportDate.setText("ประจำวันที่ " + getDate(new Date())+ " เดือน " + getMonth(new Date())+ " ปี พ.ศ. " + getYear(new Date()));
-        reportDate.setHeight("10");
+        reportDate.setContents("ประจำวันที่ " + select_day + " เดือน " + DateTimeMapping.getDisplay(select_month) + " ปี พ.ศ. " + select_year);
+        reportDate.setAlign(Alignment.CENTER);
+        reportDate.setHeight(10);
+        reportDate.setStyleName("printDetails");
         header.addMember(text);
         header.addMember(reportDate);
         
@@ -212,9 +292,11 @@ public class ReportQuotationLayout extends VLayout{
 //			reportListGrid.removeData(record);
 //		}
 //		viewButton.hide();
-		reportDate.setText("ประจำวันที่ " + getDate(searchItem.getValueAsDate())+ " เดือน " + getMonth(searchItem.getValueAsDate())+ " ปี พ.ศ. " + getYear(searchItem.getValueAsDate()));
+		reportDate.setContents("ประจำวันที่ " + select_day + " เดือน " + DateTimeMapping.getDisplay(select_month) + " ปี พ.ศ. " + select_year);
         
-		reportListGrid.fetchData(searchForm.getValuesAsCriteria());
+		Date date = DateTimeFormat.getFormat("yyyy-M-dd").parse(DateTimeMapping.getRealYear(select_year) + "-" + select_month + "-" + select_day);
+		
+		reportListGrid.fetchData(new Criterion("created_date", OperatorId.EQUALS, date));
 		reportListGrid.deselectAllRecords();
 	}
 	
@@ -223,46 +305,46 @@ public class ReportQuotationLayout extends VLayout{
 //			reportListGrid.removeData(record);
 //		}
 //		viewButton.hide();
-		reportDate.setText("ประจำวันที่ " + getDate(new Date())+ " เดือน " + getMonth(new Date())+ " ปี พ.ศ. " + getYear(new Date()));
+		reportDate.setContents("ประจำวันที่ " + select_day + " เดือน " + DateTimeMapping.getDisplay(select_month) + " ปี พ.ศ. " + select_year);
         
 		reportListGrid.fetchData(new Criterion("created_date", OperatorId.EQUALS, new Date()) );
 		reportListGrid.deselectAllRecords();
 	}
 	
-	private int getDate(Date current){
-//        Calendar cal = Calendar.getInstance();
-//        cal.setTime(current);
-//        return cal.get(Calendar.DAY_OF_MONTH);
-		return current.getDate();
-	}
-	
-	private String getMonth(Date current){
-//        Calendar cal = Calendar.getInstance();
-//        cal.setTime(current);
-//        int month = cal.get(Calendar.MONTH);
-        int month = current.getMonth();
-        switch (month) {
-        	case 0: return "มกราคม";
-        	case 1: return "กุมภาพันธ์";
-        	case 2: return "มีนาคม";
-        	case 3: return "เมษายน";
-        	case 4: return "พฤษภาคม";
-        	case 5: return "มิถุนายน";
-        	case 6: return "กรกฎาคม";
-        	case 7: return "สิงหาคม";
-        	case 8: return "กันยายน";
-        	case 9: return "ตุลาคม";
-        	case 10: return "พฤศจิกายน";
-        	case 11: return "ธันวาคม";
-        	default : return "no defined";
-        }
-	}
-	
-	private int getYear(Date current){
-//        Calendar cal = Calendar.getInstance();
-//        cal.setTime(current);
-//        return cal.get(Calendar.YEAR) + 543;
-		return current.getYear();
-	}
+//	private int getDate(Date current){
+////        Calendar cal = Calendar.getInstance();
+////        cal.setTime(current);
+////        return cal.get(Calendar.DAY_OF_MONTH);
+//		return current.getDate();
+//	}
+//	
+//	private String getMonth(Date current){
+////        Calendar cal = Calendar.getInstance();
+////        cal.setTime(current);
+////        int month = cal.get(Calendar.MONTH);
+//        int month = current.getMonth();
+//        switch (month) {
+//        	case 0: return "มกราคม";
+//        	case 1: return "กุมภาพันธ์";
+//        	case 2: return "มีนาคม";
+//        	case 3: return "เมษายน";
+//        	case 4: return "พฤษภาคม";
+//        	case 5: return "มิถุนายน";
+//        	case 6: return "กรกฎาคม";
+//        	case 7: return "สิงหาคม";
+//        	case 8: return "กันยายน";
+//        	case 9: return "ตุลาคม";
+//        	case 10: return "พฤศจิกายน";
+//        	case 11: return "ธันวาคม";
+//        	default : return "no defined";
+//        }
+//	}
+//	
+//	private int getYear(Date current){
+////        Calendar cal = Calendar.getInstance();
+////        cal.setTime(current);
+////        return cal.get(Calendar.YEAR) + 543;
+//		return current.getYear() + 543;
+//	}
 	
 }
---
+++ src/com/smart/mis/shared/DateTimeMapping.java
@@ -0,0 +1,75 @@
+package com.smart.mis.shared;
+
+import java.util.LinkedHashMap;
+
+public class DateTimeMapping {
+	static LinkedHashMap<String, String> monthMap;
+	
+	static {
+		monthMap = new LinkedHashMap<String, String>();  
+		monthMap.put("1", "มกราคม");  
+		monthMap.put("2", "กุมภาพันธ์"); 
+        monthMap.put("3", "มีนาคม");  
+        monthMap.put("4", "เมษายน"); 
+        monthMap.put("5", "พฤษภาคม");  
+        monthMap.put("6", "มิถุนายน"); 
+        monthMap.put("7", "กรกฎาคม");  
+        monthMap.put("8", "สิงหาคม"); 
+        monthMap.put("9", "กันยายน");  
+        monthMap.put("10", "ตุลาคม"); 
+        monthMap.put("11", "พฤศจิกายน");  
+        monthMap.put("12", "ธันวาคม");
+	}
+	
+	public static LinkedHashMap<String, String> getMonthValueMap() {
+        return monthMap;
+	}
+	
+	public static LinkedHashMap<String, String> getDateValueMap(int num) {
+		LinkedHashMap<String, String> dateMap = new LinkedHashMap<String, String>();  
+		for (int i = 1; i <= num; i++){
+			dateMap.put(Integer.toString(i), Integer.toString(i)); 
+		}
+        return dateMap;
+	}
+	
+	public static String getDisplay(String value) {
+        return monthMap.get(value);
+	}
+	
+	public static String getYearPast(String year, int num) {
+		int current = Integer.parseInt(year);
+		int past = current - num;
+		return Integer.toString(past);
+	}
+	
+	public static int getNumDay(String in_month, String in_year){
+		int month = Integer.parseInt(in_month);
+		int year = Integer.parseInt(in_year) - 543;
+		
+      switch (month) {
+    	case 1: return 31;
+    	case 2: {
+    		if ((year%4 == 0) && (year%100 != 0) && (year%400 != 0))
+    			return 29;
+    		else return 28;
+    		} 
+    	case 3: return 31;
+    	case 4: return 30;
+    	case 5: return 31;
+    	case 6: return 30;
+    	case 7: return 31;
+    	case 8: return 31;
+    	case 9: return 30;
+    	case 10: return 31;
+    	case 11: return 30;
+    	case 12: return 31;
+    	default : return 0;
+    }
+	}
+	
+	public static String getRealYear(String thai_year){
+		int year = Integer.parseInt(thai_year);
+		return Integer.toString(year - 543);
+	}
+}
